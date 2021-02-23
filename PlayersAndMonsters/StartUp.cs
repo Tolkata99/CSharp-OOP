87b4064fd6fd0dd8739224abe2ee8b640b048e01
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-02-23 18:11:39 +0200

    I solved PlayersAndMonsters from Inheritance !

---
+++ PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Hero hero = new Knight("gofo" ,45);
+
+            System.Console.WriteLine(hero);
+        }
+    }
+}
---
+++ PlayersAndMonsters/Wizard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    class Wizard : Hero
+    {
+        public Wizard(string name , int level) : base(name,level)
+        {
+
+        }
+
+
+    }
+}

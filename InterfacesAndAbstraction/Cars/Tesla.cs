cd84c0ad8835cdedc6581e8bb5271b0dd4de1f12
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-03-06 13:40:10 +0200

    I solved Cars in InterfacesAndAbstraction !

---
+++ InterfacesAndAbstraction/Cars/Tesla.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color,int battery)
+        {
+            Battery = battery;
+            Model = model;
+            Color = color;
+        }
+
+        public int Battery { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries";
+        }
+    }
+}

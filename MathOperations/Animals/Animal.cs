9f6cf603f0ece6c50d0bcf4123c0c43f693cb9ab
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-03-09 14:37:22 +0200

    I solved Animals from Polymorphism-Lecture !

---
+++ MathOperations/Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+
+        public string Name { get; private set; }
+
+        public string FavouriteFood { get; private set; }
+
+
+        protected Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favoriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}

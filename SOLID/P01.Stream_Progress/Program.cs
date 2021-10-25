0a10bd0fac194d56b658a5655e1ed71e1b6b4c36
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-10-25 18:50:26 +0300

    Added SOLID

---
+++ SOLID/P01.Stream_Progress/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            IStreamable file = new File("Anatoly", 50, 1000);
+            IStreamable music = new Music("Veselin", "Hushove", 10, 500);
+
+
+
+            StreamProgressInfo sPIFile = new StreamProgressInfo(file);
+            Console.WriteLine(sPIFile.CalculateCurrentPercent());
+
+            StreamProgressInfo sPIMusic = new StreamProgressInfo(music);
+            Console.WriteLine(sPIMusic.CalculateCurrentPercent());
+
+        }
+    }
+}

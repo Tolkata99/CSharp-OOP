0d1c18b70a11a7ea7f3d64dfa471260dffb2e414
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2021-10-25 18:54:38 +0300

    SOLID

---
+++ SOLID PRINCIPLES/P01.Stream_Progress/StreamProgressInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        private IStreamable streamable;
+
+        public StreamProgressInfo(IStreamable stream)
+        {
+            this.streamable = stream;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.streamable.ByteSent * 100) / this.streamable.Length;
+        }
+    }
+}

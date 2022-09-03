4284feca731a2155ceb7118514870e23197b4ccb
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-09-03 11:00:46 +0300

    Adding BookingRepository

---
+++ OOP-Hotel/Repositories/BookingRepository.cs
@@ -0,0 +1,30 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookingApp.Repositories
+{
+    internal class BookingRepository : IRepository<IBooking>
+    {
+        private List<IBooking> books;
+
+        public BookingRepository()
+        {
+            this.books = new List<IBooking>();
+        }
+
+        public void AddNew(IBooking model)
+        {
+            this.books.Add(model);
+        }
+
+        public IReadOnlyCollection<IBooking> All() => this.books;
+
+
+        public IBooking Select(string criteria) => this.books.FirstOrDefault(b => b.BookingNumber.ToString() == criteria);
+       
+    }
+}
